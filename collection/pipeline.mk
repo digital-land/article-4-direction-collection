a2864b802898a7eae9dfcc2a753b2508b61aa85f
Kishan Patel <16578929+ksp37@users.noreply.github.com>
2021-02-01 11:57:00 +0000

    Delete logs/resources and run first collection on valid data.

--- collection/pipeline.mk
+++ collection/pipeline.mk
@@ -1,12 +1,8 @@
 ARTICLE_4_DIRECTION_DATASET=$(DATASET_DIR)article-4-direction.csv
 ARTICLE_4_DIRECTION_TRANSFORMED_FILES=\
-    $(TRANSFORMED_DIR)article-4-direction/70ba4d0037d943b63a591d1b9f67e1dd9d19e6eae9b149f2625ce4b3d06f4470.csv\
-    $(TRANSFORMED_DIR)article-4-direction/eea42f85765252b684c6ebffca72a441790ba9bba63a6b1d63b80bda13dfa63d.csv
+    $(TRANSFORMED_DIR)article-4-direction/ec8a4933848f69e3635a6a6fe5154fbc8ca6db3458628d24f395962d2e172891.csv
 
-$(TRANSFORMED_DIR)article-4-direction/70ba4d0037d943b63a591d1b9f67e1dd9d19e6eae9b149f2625ce4b3d06f4470.csv: collection/resource/70ba4d0037d943b63a591d1b9f67e1dd9d19e6eae9b149f2625ce4b3d06f4470
-	$(run-pipeline)
-
-$(TRANSFORMED_DIR)article-4-direction/eea42f85765252b684c6ebffca72a441790ba9bba63a6b1d63b80bda13dfa63d.csv: collection/resource/eea42f85765252b684c6ebffca72a441790ba9bba63a6b1d63b80bda13dfa63d
+$(TRANSFORMED_DIR)article-4-direction/ec8a4933848f69e3635a6a6fe5154fbc8ca6db3458628d24f395962d2e172891.csv: collection/resource/ec8a4933848f69e3635a6a6fe5154fbc8ca6db3458628d24f395962d2e172891
 	$(run-pipeline)
 
 $(ARTICLE_4_DIRECTION_DATASET): $(ARTICLE_4_DIRECTION_TRANSFORMED_FILES)
