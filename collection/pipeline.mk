29bdab935989c3c6694935675193e5b9b09578d2
mattlucht <matlucht@gmail.com>
2021-11-11 14:30:23 +0000

    adds canterbury article 4

--- collection/pipeline.mk
+++ collection/pipeline.mk
@@ -2,12 +2,16 @@ ARTICLE_4_DIRECTION_DATASET=$(DATASET_DIR)article-4-direction.csv
 ARTICLE_4_DIRECTION_TRANSFORMED_FILES=\
     $(TRANSFORMED_DIR)article-4-direction/223b31dab8f2af16bd637db4be4e17239a7405a5f585200610c05cd64440c988.csv\
     $(TRANSFORMED_DIR)article-4-direction/368d17a72f6f47f94e3ab6832223fa01d38838b9d24c5f27c53e4704eee83c9d.csv\
+    $(TRANSFORMED_DIR)article-4-direction/5a63cf05c7ec1a74184180e1b42a71a7d6ea9071419b2f85f72fb59c2d8a07a0.csv\
     $(TRANSFORMED_DIR)article-4-direction/7fb3fd9a1f73608c71659df88e4c26f64e5ce883e5e1456b88dd14194cd1d677.csv
 
 $(TRANSFORMED_DIR)article-4-direction/223b31dab8f2af16bd637db4be4e17239a7405a5f585200610c05cd64440c988.csv: collection/resource/223b31dab8f2af16bd637db4be4e17239a7405a5f585200610c05cd64440c988
 	$(run-pipeline)
 
 $(TRANSFORMED_DIR)article-4-direction/368d17a72f6f47f94e3ab6832223fa01d38838b9d24c5f27c53e4704eee83c9d.csv: collection/resource/368d17a72f6f47f94e3ab6832223fa01d38838b9d24c5f27c53e4704eee83c9d
+	$(run-pipeline)
+
+$(TRANSFORMED_DIR)article-4-direction/5a63cf05c7ec1a74184180e1b42a71a7d6ea9071419b2f85f72fb59c2d8a07a0.csv: collection/resource/5a63cf05c7ec1a74184180e1b42a71a7d6ea9071419b2f85f72fb59c2d8a07a0
 	$(run-pipeline)
 
 $(TRANSFORMED_DIR)article-4-direction/7fb3fd9a1f73608c71659df88e4c26f64e5ce883e5e1456b88dd14194cd1d677.csv: collection/resource/7fb3fd9a1f73608c71659df88e4c26f64e5ce883e5e1456b88dd14194cd1d677
