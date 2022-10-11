d22e58cb8c8756974a689391f14ebf125f388810
Paul Downey <psd@whatfettle.com>
2022-10-11 17:45:08 +0100

    Stashed n script

---
+++ bin/n1.sh
@@ -0,0 +1,48 @@
+resource=""
+lpa="local-authority-eng:MDW"
+let e=7010002217
+while read ref
+do
+let e=e+1
+echo "article-4-direction-area,$resource,,$lpa,$ref,$e"
+done <<-!
+Brompton Lines
+Historic Rochester(11)
+Historic Rochester(12)
+Historic Rochester(7)
+Historic Rochester(6)
+Historic Rochester(4)
+Historic Rochester(5)
+Historic Rochester(3)
+Historic Rochester(2)
+Historic Rochester(1)
+Historic Rochester
+Historic Rochester(8)
+Historic Rochester(9)
+Historic Rochester(10)
+Historic Rochester(13)
+"Maidstone Road, Chatham"
+New Road Chatham(2)
+New Road Chatham(3)
+New Road Chatham(4)
+New Road Chatham(1)
+Star Hill to Sun Pier
+Gillingham Park
+Upper Upnor(1)
+Upper Upnor(2)
+Upper Upnor(6)
+Upper Upnor(5)
+Upper Upnor(8)
+Upper Upnor(11)
+Upper Upnor(10)
+Upper Upnor(9)
+Upper Upnor(7)
+Upper Bush (1)
+Upper Bush(3)
+Upper Bush(4)
+Upper Bush (2)
+Watts Avenue
+Upper Upnor(3)
+Upper Upnor(4)
+LAND BETWEEN ELMHAVEN MARINA AND CEMEX
+!
